3636a8e1fe7de88fd821a42653d6147d606f6555
Szyjszka <mszyszkowski93@gmail.com>
2017-02-24 16:13:59 +0100

    Less edges clearing.

--- src/CH/contraction_hierarchies/contracting.cpp
+++ src/CH/contraction_hierarchies/contracting.cpp
@@ -10,21 +10,10 @@ namespace RouterCH
 bool chechIfShortcudNeeded(EdgesTable& edgesTable, const int64_t u,
                            const int64_t v, const int64_t w, const Nodes &nodes)
 {
-    EdgesTable edgesTableForLocalSearch(edgesTable);
-
-    //wstawienie do tabeli duzych wartosci dla poszkiwanego skrótu żeby zobaczyć czy
-    //dijkstra znajdzie inną trasę
     Route checkedShortctut;
     checkedShortctut.cost = edgesTable[u][v] + edgesTable[v][w];
-    //PRZENIESC TO WYZEJ, PO CO ZA KAZDYM RAZEM TO USTAWIAC
-    for(const std::pair<int64_t, Edges>& ways : edgesTable)
-    for(const std::pair<int64_t, Edge>& edge : ways.second)
-    {
-        edgesTableForLocalSearch[ways.first][edge.first] = INF;
-        edgesTableForLocalSearch[edge.first][ways.first] = INF;
-    }
 
-    Route sh = dijkstra(edgesTableForLocalSearch, u, w, nodes, checkedShortctut.cost);
+    Route sh = dijkstra(edgesTable, u, w, nodes, checkedShortctut.cost);
 
     return sh.cost > checkedShortctut.cost;
 }
@@ -36,18 +25,37 @@ void contractNode(EdgesTable& edgesTable, const Node& v, const Nodes &nodes)
     {
         return;
     }
-    for(auto i = edgesTable[v.osm_id()].begin(); i != prev(edgesTable[v.osm_id()].end()); ++i)
-    for(auto j = next(i); j != edgesTable[v.osm_id()].end(); ++j)
+
+    Edges savedEdges;
+    std::vector<int64_t> connectedNodes;
+    //erase connections
+    for(std::pair<int64_t, Edge> elem : edgesTable[v.osm_id()])
     {
-        int64_t uID = i->first;
-        int64_t wID = j->first;
-        if(chechIfShortcudNeeded(edgesTable, uID, v.osm_id(), wID, nodes))
+        savedEdges[elem.first] = elem.second;
+        edgesTable[elem.first].erase(v.osm_id());
+        edgesTable[v.osm_id()].erase(elem.first);
+        connectedNodes.push_back(elem.first);
+    }
+
+    for(auto i = connectedNodes.begin(); i != prev(connectedNodes.end()); ++i)
+    for(auto j = next(i); j != connectedNodes.end(); ++j)
+    {
+
+        if(chechIfShortcudNeeded(edgesTable, *i , v.osm_id(), *j, nodes))
         {
         //dodaj skrót (u,v,w)
-            (edgesTable)[uID][wID] = (edgesTable)[uID][v.osm_id()] + (edgesTable)[v.osm_id()][wID];
-            (edgesTable)[wID][uID] = (edgesTable)[uID][v.osm_id()] + (edgesTable)[v.osm_id()][wID];
+            (edgesTable)[*i][*j] = (edgesTable)[*i][v.osm_id()] + (edgesTable)[v.osm_id()][*j];
+            (edgesTable)[*j][*i] = (edgesTable)[*i][v.osm_id()] + (edgesTable)[v.osm_id()][*j];
         }
     }
+    //Restore connections
+    for(std::pair<int64_t, Edge> elem : savedEdges)
+    {
+        edgesTable[elem.first][v.osm_id()] = elem.second;
+        edgesTable[v.osm_id()][elem.first] = elem.second;
+    }
+
+
 
 }
 
